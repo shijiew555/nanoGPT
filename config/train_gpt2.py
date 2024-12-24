cbdc3320567921c6f5384ff656550ce62df701a6
shijiew555 <shijiew555@gmail.com>
2024-12-24 15:18:36 -0800

    Update training parameters

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -4,21 +4,21 @@
 
 wandb_log = True
 wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_run_name='gpt2-short-prof'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
-block_size = 1024
+batch_size = 64
+block_size = 512
 gradient_accumulation_steps = 5 * 8
 
 # this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+max_iters = 210
+lr_decay_iters = 210
 
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 40
+eval_iters = 1
 log_interval = 10
 
 # weight decay
